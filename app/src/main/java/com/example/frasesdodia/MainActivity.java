658641e90d60bf97581f8866e9dba44fb7870d1c
edmarpurcina <edmar.purcina@gmail.com>
2021-01-05 17:00:20 -0300

    Escrevendo aplicação para mudar textView aleatoriamente

--- app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+
+        TextView text = findViewById(R.id.textView);
+        String[] frases = {
+                "Bom dia Flor do dia!",
+                "Onde você for, lá você estará!",
+                "Hakuna Matata!",
+                "Os seus problemas vcoê tem que esquecer!"
+        };
+        int numero = new Random().nextInt(4);
+
+        text.setText(frases[numero]);
+
+
+    }
+
 }
